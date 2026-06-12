b6eed1fe118f29b1c0c2337cea322629c478c286
User <udeipra@gmail.com>
2026-06-12 23:47:00 +0700

    chore: update site URL, email, and fix image extension

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,7 @@
-export const SITE_URL = 'https://dagogolf.com'; // TODO: set to the real production domain
+export const SITE_URL = 'https://www.dagogolfacademy.com/'; // TODO: set to the real production domain
 // Central place for contact details so they stay consistent site-wide.
 export const WHATSAPP_NUMBER = '6285175090700'; // +62 851-7509-0700
-export const EMAIL = 'info@dagogolf.com';
+export const EMAIL = 'dagogolfacademy@gmail.com';
 export const ADDRESS = 'Dago heritage 1917, Jl. Cigadung Raya Barat No.5, RT.05/RW.02, Cigadung, Kec. Cibeunying Kaler, Kota Bandung, Jawa Barat 40191';
 export const OPENING_HOURS = '08.00-16.00';
 
